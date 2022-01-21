ca64e8f86d79de9802eb37a55971ea7765301831
csokolove <94080789+csokolove@users.noreply.github.com>
2022-01-21 07:27:07 -0500

    wip: initializer & set-up wizard created

--- bin/node-create.js
+++ bin/node-create.js
@@ -1,1 +1,9 @@
-#!/usr/bin/env node
+#!/usr/bin/node
+
+/**
+ * @fileoverview Main CLI that is run via the node-create command.
+ * @author csokolove
+ */
+
+import { nodeCreate } from '../lib/create/create-initializer.js';
+nodeCreate();
